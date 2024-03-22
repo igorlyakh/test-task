7d2001c26ee2a52e9d900737fedc1fb8429b4555
Igor Liakh <igorlyah060@gmail.com>
2024-03-22 19:49:55 +0200

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<div>Layout</div>}
+      >
+        <Route
+          index
+          element={<div>Welcome</div>}
+        />
+        <Route
+          path="catalog"
+          element={<div>Catalog</div>}
+        />
+        <Route
+          path="favorite"
+          element={<div>Favorite</div>}
+        />
+      </Route>
+      <Route
+        path="*"
+        element={<div>404</div>}
+      />
+    </Routes>
+  );
 };
 
 export default App;
