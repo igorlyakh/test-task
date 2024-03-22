e2da0e69cf30b45460d8ddfabf8f4274b2ecb4e2
Igor Liakh <igorlyah060@gmail.com>
2024-03-22 19:35:00 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+});
